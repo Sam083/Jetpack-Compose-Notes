cffabb252901115389a7d8b71658379bf8bc784b
Samarth Gupta <98847477+Sam083@users.noreply.github.com>
2023-02-21 19:12:41 +0530

    Create first example.kt

---
+++ first example.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+class example constructor(val age: Int) {
+    
+    
+    //val cant use inside function parameters
+    fun talk(name: String)
+    {
+        val msg = "Hey $name. How are you buddy!!!just to let you know age $age is just a no"
+        println(msg)
+
+    }
+
+}
+fun main(args: Array<String>)
+{
+    val sample: example = example(22)
+    sample.talk("Samarth")
+}
