0509c85f85d51f870d7895c1e7fcf28491d7b188
Samarth Gupta <98847477+Sam083@users.noreply.github.com>
2023-02-21 19:09:58 +0530

    Add files via upload
    Inheritance in kotlin

--- example.kt
+++ example.kt
@@ -1,19 +1,42 @@
 package com.example.myapplication
 
-class example constructor(val age: Int) {
-    
-    
-    //val cant use inside function parameters
-    fun talk(name: String)
-    {
-        val msg = "Hey $name. How are you buddy!!!just to let you know age $age is just a no"
-        println(msg)
+//Inheritance in kotlin
+
+    open class Person constructor(public val age: Int) {
+
+        open fun talk(name: String) {
+            val msg = "Hey $name. This is Person class of $age"
+            println(msg)
+
+        }
 
     }
 
-}
-fun main(args: Array<String>)
-{
-    val sample: example = example(22)
-    sample.talk("Samarth")
-}
+    //Person has a constructor so we also need to make constructor in Teacher
+    class Teacher constructor(age: Int) : Person(age) {
+
+        override fun talk(name: String) {
+            val msg = "Hey $name. This is teacher class of $age"
+            println(msg)
+        }
+
+        fun teach() {
+            println("Hey!!! I am ur new teacher")
+        }
+    }
+
+
+    fun main(args: Array<String>) {
+
+        val person: Person = Person(20)
+        person.talk("Robo")
+
+        var teacher: Teacher = Teacher(50)
+        teacher.talk("Anand Sir")
+
+        teacher = Teacher(55)
+        teacher.talk("Rathi Sir")
+        teacher.teach()
+
+    }
+
