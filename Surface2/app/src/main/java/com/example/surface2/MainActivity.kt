b7374117d56b2c837c6cda2e270cea778c8b3cd8
Samarth <samarthg8383@gmail.com>
2023-02-23 15:29:39 +0530

    Update

--- Surface2/app/src/main/java/com/example/surface2/MainActivity.kt
+++ Surface2/app/src/main/java/com/example/surface2/MainActivity.kt
@@ -9,10 +9,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.surface.ui.theme.SurfaceTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,12 +30,14 @@ fun MainScreen(){
     //Parent Layout
     //match parent on width and height
     Surface(color = Color.DarkGray,
-        modifier = Modifier.fillMaxSize()){
+        modifier = Modifier.fillMaxSize(),
+        ){
 
         //Child Layout
         //wrap content on width and height
         Surface(color = Color.Cyan,
-            modifier = Modifier.wrapContentSize()) {
+            modifier = Modifier.wrapContentSize(
+                align = Alignment.TopCenter)) {
             Text(text = "Wrap Content Size",
                 style = MaterialTheme.typography.headlineMedium)
         }
