e71f601710839893d308e090aa68ea9d8b93fa09
Samarth <samarthg8383@gmail.com>
2023-02-24 09:49:00 +0530

    Update

--- DynamicContent/app/src/main/java/com/example/dynamiccontent/MainActivity.kt
+++ DynamicContent/app/src/main/java/com/example/dynamiccontent/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.dynamiccontent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dynamiccontent.ui.theme.DynamicContentTheme
@@ -18,25 +21,27 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DynamicContentTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingList(namesList)
-                }
-            }
+            GreetingList(namesList)
+
         }
     }
 }
 
 @Composable
 fun GreetingList(names: List<String>) {
-    Column() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         for(name in names)
             Greeting(name = name)
+        Button(onClick = {  }) {
+            Text(text = "Add New Name")
+        }
+
     }
+
 }
 @Composable
 fun Greeting(name:String){
